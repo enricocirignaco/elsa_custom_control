454a1e81e8352b42c71175a8f0ccf4316fbf11a2
Enrico Cirignaco <enrico.cirignaco@students.bfh.ch>
2024-10-18 10:35:27 +0200

    first commit

---
+++ src/arduino/main/main.ino
@@ -0,0 +1,15 @@
+// Pinout
+#define pasPin 34 // digital interrupt
+#define outputPin 25 // DAC output
+#define throttlePin 35 // ADC input
+#define ledPin 2 // PWM output
+
+
+void setup() {
+  pinMode(pasPin, INPUT);
+}
+
+
+void loop() {
+  
+  }
